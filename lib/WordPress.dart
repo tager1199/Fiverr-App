5e10fb812a91d549859260cfca142eab6a4737bf
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-09-15 15:36:41 +0100

    Update WordPress.dart

--- lib/WordPress.dart
+++ lib/WordPress.dart
@@ -25,10 +25,9 @@ class _PostManagerState extends State<PostManager> {
   List posts;
 
   Future<String> getPosts() async {
-    var res = await http.get(Uri.encodeFull(apiUrl + "posts?_embed"),
-        headers: {"Accept": "application/json"}
-        );
-
+    var res = await http.get(
+        Uri.encodeFull(apiUrl + "posts?_embed&per_page=10"),
+        headers: {"Accept": "application/json"});
 
     // fill our posts list with results and update state
     setState(() {
@@ -46,63 +45,66 @@ class _PostManagerState extends State<PostManager> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: (MediaQuery.of(context).size.width),
-      height: 10000.0,
-      child: Scaffold(
-          body: ListView.builder(
-        itemCount: posts == null ? 0 : posts.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (posts[index]["categories"].contains(widget.category)) {
-            return GestureDetector(
-              child: Card(
-                margin: EdgeInsets.all(5.0),
-                color: Color(0xFFA6ACAF), // card color
-                child: Column(
-                  children: <Widget>[
-                    Card(
-                      child: Column(
-                        children: <Widget>[
-                          new CachedNetworkImage(
-                            imageUrl: posts[index]["_embedded"]
-                                ["wp:featuredmedia"][0]["source_url"],
-                            placeholder: (context, url) =>
-                                CircularProgressIndicator(),
-                            errorWidget: (context, url, error) =>
-                                Icon(Icons.error),
-                          ),
-                          new Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: new ListTile(
-                              title: new Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                                  child: new Text(
-                                      posts[index]["title"]["rendered"])),
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: (MediaQuery.of(context).size.width),
+        height: 10000.0,
+        child: Scaffold(
+            body: ListView.builder(
+          itemCount: posts == null ? 0 : posts.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (posts[index]["categories"].contains(widget.category)) {
+              return GestureDetector(
+                child: Card(
+                  margin: EdgeInsets.all(5.0),
+                  color: Color(0xFFA6ACAF), // card color
+                  child: Column(
+                    children: <Widget>[
+                      Card(
+                        child: Column(
+                          children: <Widget>[
+                            new CachedNetworkImage(
+                              imageUrl: posts[index]["_embedded"]
+                                  ["wp:featuredmedia"][0]["source_url"],
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator(),
+                              errorWidget: (context, url, error) =>
+                                  Icon(Icons.error),
                             ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                            new Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: new ListTile(
+                                title: new Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(vertical: 10.0),
+                                    child: new Text(
+                                        posts[index]["title"]["rendered"])),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              onTap: () {
-                WebView(
-                  initialUrl: posts[index]["guid"]["rendered"],
-                  javascriptMode: JavascriptMode.unrestricted,
-                );
-                //launch(posts[index]["guid"]["rendered"]);
-              },
-            );
-          } else {
-            return Column(
-              children: [
-                Text("Sorry but we do not have any coupons in that category")
-              ],
-            );
-          }
-        },
-      )),
+                onTap: () {
+                  WebView(
+                    initialUrl: posts[index]["guid"]["rendered"],
+                    javascriptMode: JavascriptMode.unrestricted,
+                  );
+                  //launch(posts[index]["guid"]["rendered"]);
+                },
+              );
+            } else {
+              return Column(
+                children: [
+                  Text("Sorry but we do not have any coupons in that category")
+                ],
+              );
+            }
+          },
+        )),
+      ),
     );
   }
 }
